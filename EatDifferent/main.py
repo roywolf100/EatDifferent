73a6d9f81da6de4e4008dc2589a0f1a2fe42888f
roywolf100 <roywolf100@gmail.com>
2025-02-03 00:05:06 +0200

    Add files via upload

---
+++ EatDifferent/main.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+   return render_template('index.html')
+
+@app.route("/signup")
+def signup():
+   return render_template('signup.html')
+
+@app.route("/login")
+def login():
+   return render_template('login.html')
+
+
+def main():
+   app.run(port=int(os.environ.get('PORT', 5000)))
+
+if __name__ == "__main__":
+   main()
